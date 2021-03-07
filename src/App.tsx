41a87d3c6c061237496f19dca88a568b681e1f2d
Amirhossein Andohkosh <4315615+amirha97@users.noreply.github.com>
2021-03-07 06:56:04 +0000

    Improve tech tag spacing and update project photos to fit the container better and use custom positioning to crop them in a specific way

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ const theme = extendTheme({
     styles: {
         global: (props) => ({
             body: {
-                fontFamily: "body",
+                fontFamily: "body", // TODO: make font Robot
                 color: mode("gray.800", "whiteAlpha.900")(props),
                 // bg: mode("white", "gray.800")(props), //
                 bg: mode("brand.primary", "gray.800")(props),
@@ -64,7 +64,7 @@ const App = () => {
                     <Flex maxW="1140px" mx="auto">
                         <Box color="white" py="100" pr="100">
                             <Heading as="h1">Hey 👋, I’m Amirhossein Andohkosh</Heading>
-                            <Box pt="30">Senior Software Engineer at
+                            <Box pt="30" fontSize="18">Senior Software Engineer at
                                 {' '}
                                 <LightLink href="https://upsidetechnology.co/" rel="noopener noreferrer">
                                     Upside Technologies
@@ -133,14 +133,14 @@ const App = () => {
                             <BsDisplay size={50}/>
                         </Flex>
                         <Box>
-                            <Tag m={3} size="lg">React</Tag>
-                            <Tag m={3} size="lg">TypeScript</Tag>
-                            <Tag m={3} size="lg">JSS</Tag>
-                            <Tag m={3} size="lg">Webpack</Tag>
-                            <Tag m={3} size="lg">Cypress</Tag>
-                            <Tag m={3} size="lg">GraphQL</Tag>
-                            <Tag m={3} size="lg">Storybook</Tag>
-                            <Tag m={3} size="lg">jQuery</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">React</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">TypeScript</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">JSS</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">Webpack</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">Cypress</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">GraphQL</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">Storybook</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">jQuery</Tag>
                         </Box>
                     </TechCard>
 
@@ -150,16 +150,16 @@ const App = () => {
                             <BsDisplay size={50}/>
                         </Flex>
                         <Box>
-                            <Tag m={3} size="lg">Go</Tag>
-                            <Tag m={3} size="lg">Python</Tag>
-                            <Tag m={3} size="lg">C++</Tag>
-                            <Tag m={3} size="lg">NodeJS</Tag>
-                            <Tag m={3} size="lg">ELPS</Tag>
-                            <Tag m={3} size="lg">gRPC</Tag>
-                            <Tag m={3} size="lg">PHP</Tag>
-                            <Tag m={3} size="lg">Perl</Tag>
-                            <Tag m={3} size="lg">PostgreSQL</Tag>
-                            <Tag m={3} size="lg">Django</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">Go</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">Python</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">C++</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">NodeJS</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">ELPS</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">gRPC</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">PHP</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">Perl</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">PostgreSQL</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">Django</Tag>
                         </Box>
                     </TechCard>
 
@@ -169,12 +169,13 @@ const App = () => {
                             <BsDisplay size={50}/>
                         </Flex>
                         <Box>
-                            <Tag m={3} size="lg">Terraform</Tag>
-                            <Tag m={3} size="lg">AWS</Tag>
-                            <Tag m={3} size="lg">GCP</Tag>
-                            <Tag m={3} size="lg">Kubernetes</Tag>
-                            <Tag m={3} size="lg">Docker</Tag>
-                            <Tag m={3} size="lg">Helm</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">Terraform</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">AWS</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">GCP</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">Kubernetes</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">Docker</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">Helm</Tag>
+                            <Tag mx={1} my={2} borderRadius="full" size="lg">CI/CD</Tag>
                         </Box>
                     </TechCard>
 
@@ -183,7 +184,7 @@ const App = () => {
                 <Box pt="5" color="white" maxW="1140px" mx="auto">
                     <Heading as="h2" py="10">Projects</Heading>
                     <Flex py="50">
-                        <Image src={lispExtensionScreenshot} w="45%" objectFit="contain"></Image>
+                        <Image src={lispExtensionScreenshot} w="45%" objectFit="cover" align="0 100%"/>
                         <Spacer/>
                         <Box w="45%">
                             <Heading as="h3">ELPS VSCode Extension</Heading>
@@ -196,7 +197,7 @@ const App = () => {
                                 highlight our code.
 
                                 <br/><br/>
-                                You can read more about this project here.
+                                Read more about this project →
                             </Text>
                         </Box>
                     </Flex>
@@ -219,7 +220,7 @@ const App = () => {
                             </Text>
                         </Box>
                         <Spacer/>
-                        <Image src={pedestrianTracker} w="45%" objectFit="contain"></Image>
+                        <Image src={pedestrianTracker} w="45%" objectFit="cover" align="75% 0"/>
                     </Flex>
                 </Box>
 
