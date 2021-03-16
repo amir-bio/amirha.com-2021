0ef8a3f8a716c9ef642e56d00a357aae1e7c5dc6
Amirhossein Andohkosh <4315615+amirha97@users.noreply.github.com>
2021-03-16 07:53:47 +0000

    Fix cypress spec config in CI
    Setup CI pnpm caching
    Upload cypress videos and screenshots in case of failures
    Add cypress videos and screenshots to gitignore

--- cypress/integration/landingpage/landingpage.spec.js
+++ cypress/integration/landingpage/landingpage.spec.js
@@ -1,8 +1,9 @@
 describe('Test landing page with visual testing', () => {
   it('Loads the landing page', () => {
-    // Load the page or perform any other interactions with the app.
     cy.visit('/');
 
+    cy.get('h1').contains('Hey 👋, I’m Amirhossein Andohkosh').should('be.visible')
+    cy.get('img[alt="Amirhossein Andohkosh"]').should('be.visible')
     // Take a snapshot for visual diffing - widths correspond to a mobile phone, ipad, 11" laptop, 15" laptop
     cy.percySnapshot('Landing page', { widths: [320, 768, 1366, 1440] });
   });
